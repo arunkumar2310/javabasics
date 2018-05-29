374f84fa51932f09c153d3fdb1ff8e91b271881c
arunkumar2310 <arunc231097@gmail.com>
2018-05-29 15:19:18 +0530

    basics

--- src/com/basic/DataTypeExample.java
+++ src/com/basic/DataTypeExample.java
@@ -1,7 +1,7 @@
 package com.basic;
 
 public class DataTypeExample {
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		String firstName="Arun";
 		String lastName="Kumar";
 		System.out.println(firstName+""+lastName);
