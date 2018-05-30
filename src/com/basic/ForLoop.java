29043b05715c0f13f2a7178f7b6e83f842eb49fb
arunkumar2310 <arunc231097@gmail.com>
2018-05-30 12:21:52 +0530

    simple for loop

---
+++ src/com/basic/ForLoop.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class ForLoop {
+public static void main(String s[]){
+   int var1=10,i=0;
+   for(;i<var1;i++)
+   {
+	   System.out.println("Roll number: "+(i+1));
+   }
+   if(i==10)
+   {
+	   System.out.println("End of for loop");
+   }
+}
+}
