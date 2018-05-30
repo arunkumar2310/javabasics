2441ec019b0e8dbcd00e9840fab0d18f3521abe2
arunkumar2310 <arunc231097@gmail.com>
2018-05-30 12:36:07 +0530

    program for switch case

---
+++ src/com/basic/SwitchCase.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class SwitchCase {
+public static void main(String s[]) {
+	String myName="Arun";
+	int myNum=2;
+	switch(myName)
+	{
+	    case "Arun":
+		    System.out.println(myName);
+		    break;
+	    case "Kumar":
+	    	System.out.println("Kumar");
+	    	break;
+	      	
+	}
+	System.out.println("My number:"+myNum);
+}
+}
