eb182c0877be00252f439f004bf2f1a4c8f9d80e
arunkumar2310 <arunc231097@gmail.com>
2018-05-30 10:01:32 +0530

    program with character datatypes

---
+++ src/com/basic/DifferentDatatypes.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class DifferentDatatypes {
+	public static void main(String s[])
+	{
+	
+	int var3=15;
+	byte var1=5;
+	short var2=10;
+    long var4=20;
+      
+   
+    if(var1<var2)
+    {
+    	System.out.println("my values:"+"\n"+var1+"\n"+var2);
+    }
+    
+    String myValue="my_datas";
+    String output=var3+myValue+var4+var1;
+    System.out.println(output);
+	}}
