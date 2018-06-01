5edb4315c052f2e747c4108aff7dfc55a344940e
arunkumar2310 <arunc231097@gmail.com>
2018-06-01 11:06:42 +0530

    static

--- src/com/worksheet_answers/SavingsAccount.java
+++ src/com/worksheet_answers/SavingsAccount.java
@@ -1,7 +1,7 @@
 package com.worksheet_answers;
 
 public class SavingsAccount {
-    static double savingsbalance;
+    private double savingsbalance;
 	static double annualInterestRate;
 
 	public double getAnnualInterestRate() {
@@ -17,11 +17,11 @@ public class SavingsAccount {
 	}
 
 	public void setSavingsbalance(double savingsbalance) {
-		SavingsAccount.savingsbalance = savingsbalance;
+		this.savingsbalance = savingsbalance;
 	}
 	public double calculateMonthlyInterest()
 	{
-		return ((savingsbalance*annualInterestRate)/12);
+		return (((savingsbalance*annualInterestRate)/12)+savingsbalance);
 	}
     public void modifyInterestRate(double annualInterestRate)
     {
@@ -31,16 +31,16 @@ public class SavingsAccount {
     {
     	SavingsAccount saver1=new SavingsAccount();
     	SavingsAccount saver2=new SavingsAccount();
-    	saver1.setSavingsbalance(2000.00);
+    	saver1.setSavingsbalance(3000.00);
     	saver1.setAnnualInterestRate(0.04);
-    	saver2.setAnnualInterestRate(0.04);
-    	saver2.setSavingsbalance(3000.00);
+    	//saver2.setAnnualInterestRate(0.04);
+    	saver2.setSavingsbalance(2000.00);
     	System.out.println("Monthly interest for saver1="+saver1.calculateMonthlyInterest());
     	System.out.println("Monthly interest for saver2="+saver2.calculateMonthlyInterest());
     	saver1.modifyInterestRate(0.05);
-    	saver2.modifyInterestRate(0.05);
-    	saver1.setAnnualInterestRate(0.05);
-    	saver2.setAnnualInterestRate(0.05);
+    	//saver2.modifyInterestRate(0.05);
+    	//saver1.setAnnualInterestRate(0.05);
+    	//saver2.setAnnualInterestRate(0.05);
     	System.out.println("After increasing interest rate"+"\n"+"Monthly interest for saver1="+saver1.calculateMonthlyInterest());
     	System.out.println("Monthly interest for saver2="+saver2.calculateMonthlyInterest());
     	
