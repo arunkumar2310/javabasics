205e4968c44284581f3bd0d02756e830f235b194
arunkumar2310 <arunc231097@gmail.com>
2018-06-01 10:11:31 +0530

    program to calculate monthly interest rate

---
+++ src/com/worksheet_answers/SavingsAccount.java
@@ -0,0 +1,48 @@
+package com.worksheet_answers;
+
+public class SavingsAccount {
+	private double savingsbalance;
+	private double annualInterestRate;
+
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+
+	public double getSavingsbalance() {
+		return savingsbalance;
+	}
+
+	public void setSavingsbalance(double savingsbalance) {
+		this.savingsbalance = savingsbalance;
+	}
+	public double calculateMonthlyInterest()
+	{
+		return ((savingsbalance*annualInterestRate)/12);
+	}
+    public void modifyInterestRate(double annualInterestRate)
+    {
+    	this.annualInterestRate=annualInterestRate;
+    }
+    public static void main(String[] args)
+    {
+    	SavingsAccount saver1=new SavingsAccount();
+    	SavingsAccount saver2=new SavingsAccount();
+    	saver1.setSavingsbalance(2000.00);
+    	saver1.setAnnualInterestRate(0.04);
+    	saver2.setAnnualInterestRate(0.04);
+    	saver2.setSavingsbalance(3000.00);
+    	System.out.println("Monthly interest for saver1="+saver1.calculateMonthlyInterest());
+    	System.out.println("Monthly interest for saver2="+saver2.calculateMonthlyInterest());
+    	saver1.modifyInterestRate(0.05);
+    	saver2.modifyInterestRate(0.05);
+    	saver1.setAnnualInterestRate(0.05);
+    	saver2.setAnnualInterestRate(0.05);
+    	System.out.println("After increasing interest rate"+"\n"+"Monthly interest for saver1="+saver1.calculateMonthlyInterest());
+    	System.out.println("Monthly interest for saver2="+saver2.calculateMonthlyInterest());
+    	
+    }
+}
