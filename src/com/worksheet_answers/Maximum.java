ef9cbe782bd62a4852f06044d8efef44f6db801a
arunkumar2310 <arunc231097@gmail.com>
2018-06-04 11:39:56 +0530

    PROGRAM TO FIND MAXIMUM NUMBER IN AN ARRAY

--- src/com/worksheet_answers/Maximum.java
+++ src/com/worksheet_answers/Maximum.java
@@ -1,28 +1,25 @@
 package com.worksheet_answers;
 
 public class Maximum {
-      private int a[]= {3,4,5,2,33,24,2};
-      public void order()
-      {
-    	  for(int i=0;i<a.length;i++)
-    	  {
-    		  for(int j=0;j<a.length;j++)
-    		  {
-    			  if(a[i]<=a[j])
-    			  {
-    				  int temp=a[i];
-    				  a[i]=a[j];
-    				  a[j]=temp;
-    		      }
-    		  
-    	      }
-          }
-    	  System.out.println("Maximum="+a[a.length-1]);
-    	  System.out.println("Minimum="+a[0]);
-      }
-      public static void main(String args[])
-      {
-    	  Maximum m1=new Maximum();
-    	  m1.order();
-      }
+	private int a[] = { 3, 4, 5, 2, 33, 24, 2 };
+
+	public void order() {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				if (a[i] <= a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+
+			}
+		}
+		System.out.println("Maximum=" + a[a.length - 1]);
+		System.out.println("Minimum=" + a[0]);
+	}
+
+	public static void main(String args[]) {
+		Maximum m1 = new Maximum();
+		m1.order();
+	}
 }
