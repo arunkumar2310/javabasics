21771f5278f974dda47d66e63a11c1b3dde217cb
arunkumar2310 <arunc231097@gmail.com>
2018-06-12 10:59:53 +0530

    PRP Problems

---
+++ src/loops/AddDigits.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class AddDigits {
+	public static void main(String args[]) {
+		int input=1234,sum=0,rem=0;
+		for(int i=0;input!=0;i++) {
+			rem=input%10;
+			input=input/10;
+			sum=sum+rem;
+		}
+		System.out.println(sum);
+	}
+}
---
+++ src/loops/Even.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class Even {
+	public static void main(String args[]) {
+		for(int i=23;i<57;i++) {
+			if(i%2==0)
+			System.out.println(i);
+		}
+	}
+}
