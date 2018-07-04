f6e4ec1d0f97f1ab09b6747a91fe4ba27eadcc1c
arunkumar2310 <arunc231097@gmail.com>
2018-07-04 17:34:44 +0530

    example for class array list

---
+++ src/com/prp/ArrayList1.java
@@ -0,0 +1,34 @@
+package com.prp;
+import java.util.*;
+import com.prp.Employee;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		
+		Employee emp1=new Employee(1,"Arun1","abcd1",6000);
+		Employee emp2=new Employee(2,"Arun2","abcd2",6050);
+		Employee emp3=new Employee(3,"Arun3","abcd3",6090);
+		Employee emp4=new Employee(4,"Arun4","abcd4",600);
+		Employee[] arr= {emp1,emp2,emp3,emp4};
+		
+		List<Employee> empList=new ArrayList<Employee>(Arrays.asList(arr));
+		empList.add(new Employee(5,"ak","aaa",87));
+		System.out.println(empList);
+		
+		Comparator<Employee> comparator=new Comparator<Employee>() {
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				return Integer.compare(o1.getSalary(),o2.getSalary());//o2 ,o1 sorts in descending order
+			}
+		};
+		Collections.sort(empList,comparator);
+		Iterator<Employee> iter=empList.iterator();
+		while(iter.hasNext()) {
+			Employee em=iter.next();
+		System.out.println(em.getId()+" "+em.getName()+" "+em.getAddress()+" "+em.getSalary()); 
+		
+		}
+	}
+
+}
---
+++ src/com/prp/Employee.java
@@ -0,0 +1,47 @@
+package com.prp;
+
+public class Employee {
+	int id;
+	String name;
+	String address;
+	int salary;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Employee(int id, String name, String address, int salary) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.salary = salary;
+	}
+}
