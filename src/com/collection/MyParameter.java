20c5d6a38e1b8e4b5cfa83b73494e4bfc0da0fc4
arunkumar2310 <arunc231097@gmail.com>
2018-07-04 17:35:27 +0530

    simple collection example

---
+++ src/com/collection/MyParameter.java
@@ -0,0 +1,13 @@
+package com.collection;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyParameter {
+	
+	public static void main(String args[]) {
+		ArrayList<Number> myInt=new ArrayList<Number>();
+		myInt.add( 1.1234567890000023356702);
+		myInt.add(3000);
+		System.out.println(myInt);
+	}
+}
