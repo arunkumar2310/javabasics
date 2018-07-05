7c7ad5939620d775a8ded3ce33636c54e2890252
arunkumar2310 <arunc231097@gmail.com>
2018-07-05 11:46:12 +0530

    program to find string code

--- src/com/prp/StringCode.java
+++ src/com/prp/StringCode.java
@@ -3,35 +3,53 @@ import java.util.Scanner;
 
 public class StringCode {
 	public static String findStringCode(String input) {
-		String output;
+		input=input.toUpperCase();
+		String output1="";
 		String a[]=input.split(" ");
-		int output1[]=new int[a.length];
+		int sum=0,m=0,n=0,e=0;
 		for(int i=0;i<a.length;i++) {
 			char b[]=a[i].toCharArray();
 			if(b.length%2==0) {
-				int x[]=new int[b.length];
-				int y=0;
+				System.out.println("  EVE");
 				for(int j=0;j<b.length/2;j++) {
-				int m=(int)b[j];
-				int n=(int)b[b.length-1-j];
-				int e=m-n;
-				if(e<0) {
-					x[y]=-e;
-					y++;
-				}
-				else {
-					x[y]=e;
-					y++;
-				}
-				}
+				 m=(int)b[j];
+				 n=(int)b[b.length-1-j];
+			      e=(m-n);
+				    sum=sum+e;
+				   }
+				//System.out.println(m+" "+n+" "+" "+e+" "+sum);
 			}
-		}
-		return output;
+			
+			else {
+				System.out.println("  ODD");
+				for(int j=0;j<=b.length/2;j++) {
+				
+					if(j==b.length/2) {
+						 e=(int)b[b.length/2]-64;
+					}
+					else {
+						 m=(int)b[j];
+						 n=(int)b[b.length-1-j];
+						 e=(m-n);
+					}
+				    sum=sum+e;
+				    //System.out.println(b.length/2+" "+j+" "+m+" "+n+" "+" "+e+" "+sum);
+				}
+			    }
+			
+			output1=output1+sum+"";
+			sum=0;
+			}
+		
+		return output1;
 	}
 	public static void main(String args[]) {
+	@SuppressWarnings("resource")
 	Scanner scan=new Scanner(System.in);
 	String input1=scan.nextLine();
+	@SuppressWarnings("unused")
 	StringCode obj=new StringCode();
-	obj.findStringCode(input1);
+	String a=findStringCode(input1);
+	System.out.println(a);
 	}
 }
